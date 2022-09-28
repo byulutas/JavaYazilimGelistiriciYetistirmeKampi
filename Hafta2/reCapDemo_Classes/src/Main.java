9b012f73dc36a49d934c53f48937847052bd0295
BAYRAM ULUTAŞ <57443241+byulutas@users.noreply.github.com>
2022-09-28 19:04:04 +0300

    Ödev Yüklendi.

---
+++ Hafta2/reCapDemo_Classes/src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        Dortİslem dortİslem=new Dortİslem();
+        int sonuc=dortİslem.Topla(3,4);
+        System.out.println(sonuc);
+    }
+}
