53a7b0d31912d1145374b642e296839a28c930c2
BAYRAM ULUTAŞ <57443241+byulutas@users.noreply.github.com>
2022-09-28 19:41:23 +0300

    Ödev Yüklendi.

---
+++ Hafta2/classesWithAttributes/src/Product.java
@@ -0,0 +1,84 @@
+public class Product {
+
+    public  Product(int id,String name,String description,double price,int stockAmount,String renk){
+        System.out.println("Yapıcı blok çalıştı.");
+        this._id=id;
+        this._renk=renk;
+        this._name=name;
+        this._price=price;
+        this._description=description;
+        this._stockAmount=stockAmount;
+    }
+    public  Product(){
+
+    }
+
+    //attribute | field
+    private int _id;
+    private String _name;
+    private String _description;
+    private double _price;
+    private int _stockAmount;
+    private String _renk;
+    private String _kod;
+
+
+    //getter
+    public  int getId(){
+        return _id;
+    }
+    //setter
+    public  void  setId(int id){
+        _id=id;
+    }
+
+
+    public  String getName(){
+        return _name;
+    }
+
+    public  void  setName(String name){
+        _name=name;
+    }
+
+
+    public  String getDescription(){
+        return _description;
+    }
+
+    public  void  setDescription(String description){
+        _description=description;
+    }
+
+
+    public  int getPrice(){
+        return _id;
+    }
+
+    public  void  setPrice(int price){
+        _price=price;
+    }
+
+
+    public  int getStockAmount(){
+        return _stockAmount;
+    }
+
+    public  void  setStockAmount(int stockAmount){
+        _stockAmount=stockAmount;
+    }
+
+
+    public  String getrenk(){
+        return _renk;
+    }
+
+    public  void  setRenk(String renk){
+        _renk=renk;
+    }
+
+    public  String getKod(){
+        return this._name.substring(0,1)+_id;
+    }
+
+}
